e8043e038b255fc8b9ce9b2e8f20a48f6ff4e42a
Beno!t POLASZEK <bpolaszek@gmail.com>
2022-07-06 10:28:30 +0200

    chore: update tests

--- src/filters/tests/daterange-filter.test.js
+++ src/filters/tests/daterange-filter.test.js
@@ -5,12 +5,12 @@ import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-DateRangeFilter.userTimezone = 'Europe/Paris';
+DateRangeFilter.userTimezone = 'Europe/Tallinn';
 
 it('denormalizes an input object', () => {
   const filter = DateRangeFilter.denormalize({
-    after: '2021-06-15T22:00:00Z',
-    before: '2021-06-17T21:59:59Z',
+    after: '2021-06-15T21:00:00Z',
+    before: '2021-06-17T20:59:59Z',
   });
   expect(filter).toBeInstanceOf(DateRangeFilter);
   expect(filter.after).toBe('2021-06-16');
@@ -19,14 +19,14 @@ it('denormalizes an input object', () => {
 
 it('partially denormalizes an input object', () => {
   let filter = DateRangeFilter.denormalize({
-    before: '2021-06-17T21:59:59Z',
+    before: '2021-06-17T20:59:59Z',
   });
   expect(filter).toBeInstanceOf(DateRangeFilter);
   expect(filter.after).toBeNull();
   expect(filter.before).toBe('2021-06-17');
 
   filter = DateRangeFilter.denormalize({
-    after: '2021-06-15T22:00:00Z',
+    after: '2021-06-15T21:00:00Z',
   });
   expect(filter).toBeInstanceOf(DateRangeFilter);
   expect(filter.after).toBe('2021-06-16');
@@ -36,8 +36,8 @@ it('partially denormalizes an input object', () => {
 it('normalizes the filter', () => {
   const filter = new DateRangeFilter({after: '2021-06-16', before: '2021-06-18'});
   expect(filter.normalize()).toStrictEqual({
-    after: '2021-06-15T22:00:00Z',
-    before: '2021-06-18T21:59:59Z',
+    after: '2021-06-15T21:00:00Z',
+    before: '2021-06-18T20:59:59Z',
   });
 });
 
@@ -45,10 +45,10 @@ it('partially normalizes the filter', () => {
   let filter = new DateRangeFilter({before: '2021-06-18'});
   expect(filter.normalize()).toStrictEqual({
     after: null,
-    before: '2021-06-18T21:59:59Z',
+    before: '2021-06-18T20:59:59Z',
   }); filter = new DateRangeFilter({after: '2021-06-16'});
   expect(filter.normalize()).toStrictEqual({
-    after: '2021-06-15T22:00:00Z',
+    after: '2021-06-15T21:00:00Z',
     before: null,
   });
 });
