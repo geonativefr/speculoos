1d6ac7c0cc1506cef72fce51698aba25f8a1a66e
Beno!t POLASZEK <bpolaszek@users.noreply.github.com>
2022-11-07 14:56:38 +0100

    Feat: Pager (#31)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ export * from './clone';
 export * from './filters';
 export * from './hydra';
 export * from './mercure';
+export * from './pager';
 export * from './store';
 export * from './vulcain';
